a11c368f3c8e17e55502b13418c32d6ef4574d92
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2025-04-02 09:18:37 +0800

    Commited the code to test the singpass connectivity

---
+++ src/main/java/com/example/singpassclient/SingpassclientApplication.java
@@ -0,0 +1,40 @@
+package com.example.singpassclient;
+
+import org.apache.cxf.interceptor.LoggingInInterceptor;
+import org.apache.cxf.interceptor.LoggingOutInterceptor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.cxf.jaxrs.client.WebClient;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+public class SingpassclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingpassclientApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runClient() {
+		return args -> {
+			try{
+				WebClient client = WebClient.create("https://id.singpass.gov.sg/.well-known/keys");
+
+				client.getHeaders().remove("Accept");
+				client.accept("application/json");
+				System.out.println("Headers: "+client.getHeaders());
+
+				client.getConfiguration().getInInterceptors().add(new LoggingInInterceptor());
+				client.getConfiguration().getOutInterceptors().add(new LoggingOutInterceptor());
+
+				String response = client.get(String.class);
+				System.out.println("Response from Singpass: "+response);
+			} catch(Exception exception){
+				exception.printStackTrace();
+			}
+		};
+	}
+
+}
---
+++ src/test/java/com/example/singpassclient/SingpassclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.singpassclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingpassclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
